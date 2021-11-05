54ed88befe4a5b04c314aa41b69f02c8c4d20710
Henriqueapan <84114633+Henriqueapan@users.noreply.github.com>
2021-11-04 21:03:49 -0300

    Corrigidos erros com divisão por 0 e outros

--- js/script.js
+++ js/script.js
@@ -9,18 +9,33 @@ function calcular(op, val) {
             tela.value = ''
             buff = 0            
         }
-        else if (val != 'res' && tela.value != '' && val != '.'){
-            (isNaN(tela.value[(tela.value.length)-1])) ? 
-                tela.value = tela.value.replace(/.$/, val) : 
-                (tela.value += val); buff=0;
+        else if (val != 'res' && tela.value != '' && tela.value != '-' && val != '.'){
+            if(buff != -1){
+                (isNaN(tela.value[(tela.value.length)-1])) ? 
+                    tela.value = tela.value.replace(/.$/, val) : 
+                    (tela.value += val); buff=0;
+            }else{
+                buff = 0;
+                tela.value = buff+val;
+            }
         }
         else if (val === '.' && !(buff0.includes('.'))){
-            tela.value += val
-            buff += val
+            if(buff!=-1){
+                tela.value += val
+                buff += val
+            }else {
+                buff = 0;
+                tela.value = buff + val;
+            }
         }
         else if (val === 'res')/*resultado (=)*/{
-            tela.value = eval(tela.value)
-            buff = tela.value
+            if(tela.value.includes('/0')){
+                tela.value = 'Operação inválida (div. por 0)';
+                buff = -1;
+            }else if (tela.value != ''){
+                tela.value = eval(tela.value)
+                buff = tela.value
+            }
         }
         if ((val == '-') && (tela.value == '')) {
             tela.value += '-'
@@ -28,9 +43,19 @@ function calcular(op, val) {
             //console.log(buff)
         }
 
+        if((tela.value == '-') && (val == '+')){
+            tela.value = '';
+            buff = 0;
+        }
+
     }else {
-        tela.value += val
-        buff += val
-        //console.log(buff)
+        if(buff!=-1){
+            tela.value += val
+            buff += val
+            //console.log(buff)
+        }else {
+            buff = 0;
+            tela.value = val;
+        }
     }
 }
